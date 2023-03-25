9e0d001e21cd4ad15b0388e6d0a7d58b4a500692
Forte-NaoP <bae9750@gmail.com>
2023-03-25 23:36:18 +0900

    Change return type for result handling

--- src/database_handler.rs
+++ src/database_handler.rs
@@ -9,7 +9,13 @@ use log::{error, info, warn};
 
 pub enum DBError {
     LibError(rusqlite::ErrorCode),
-    OperationFailed(String),
+    TitleAlreadyUsed,
+    TitleNotFound,
+}
+
+pub enum DBSuccess {
+    NewUrl,
+    ExistUrl,
 }
 
 pub async fn initialize(conn: &Connection) -> Result<()> {
@@ -45,26 +51,30 @@ pub async fn initialize(conn: &Connection) -> Result<()> {
 // 현재 여러 url이 같은 정답 갖는 경우 비허용
 // 추후 중복 허용 필요가능성 있음
 
-pub async fn add_title_with_url(conn: &Connection, url: String, title: String) -> Result<(), DBError> {
+pub async fn add_title_with_url(conn: &Connection, url: String, title: String) -> Result<DBSuccess, DBError> {
     conn.call(move |conn| {
         let _url_id = conn.query_row("SELECT id FROM url WHERE url = (?1)", params![url], |row| row.get::<usize, u64>(0));
         let _title_id = conn.query_row("SELECT id FROM title WHERE title = (?1)", params![title], |row| row.get::<usize, u64>(0));
         match _title_id {
-            Ok(_) => Err(DBError::OperationFailed(String::from("Title Already Used"))),
+            Ok(_) => Err(DBError::TitleAlreadyUsed),
             Err(_) => {
                 let tx = conn.transaction().unwrap();
                 tx.execute("INSERT INTO title (title) VALUES (?1)", params![title]).unwrap();
                 let title_id = tx.query_row("SELECT last_insert_rowid()", params![], |row| row.get::<usize, u64>(0)).unwrap();
-                let url_id = match _url_id {
-                    Ok(url_id) => url_id,
+                let (url_type, url_id) = match _url_id {
+                    Ok(url_id) => (0, url_id),
                     Err(_) => {
                         tx.execute("INSERT INTO url (url) VALUES (?1)", params![url]).unwrap();
-                        tx.query_row("SELECT last_insert_rowid()", params![], |row| row.get::<usize, u64>(0)).unwrap()
+                        (1, tx.query_row("SELECT last_insert_rowid()", params![], |row| row.get::<usize, u64>(0)).unwrap())
                     }
                 };
                 tx.execute("INSERT INTO url_title (url_id, title_id) VALUES (?1, ?2)", params![url_id, title_id]).unwrap();
                 tx.commit().unwrap();
-                Ok(())
+                if url_type == 1 {
+                    Ok(DBSuccess::NewUrl)
+                } else {
+                    Ok(DBSuccess::ExistUrl)
+                }
             }
         }
     }).await
@@ -79,12 +89,12 @@ pub async fn add_alias_with_title(conn: &Connection, title: String, alias: Strin
         let _alias_id = conn.query_row("SELECT id FROM title WHERE title = (?1)", params![alias], |row| row.get::<usize, u64>(0));
         let _title_id = conn.query_row("SELECT id FROM title WHERE title = (?1)", params![title], |row| row.get::<usize, u64>(0));
         match _alias_id {
-            Ok(_) => return Err(DBError::OperationFailed(String::from("Alias Already Used"))),
+            Ok(_) => return Err(DBError::TitleAlreadyUsed),
             Err(_) => {
                 let tx = conn.transaction().unwrap();
                 let title_id = match _title_id {
                     Ok(title_id) => title_id,
-                    Err(_) => return Err(DBError::OperationFailed(String::from("Title Not Found")))
+                    Err(_) => return Err(DBError::TitleNotFound)
                 };
                 let url_id = tx.query_row("SELECT url_id FROM url_title WHERE title_id = (?1)", params![title_id], |row| row.get::<usize, u64>(0)).unwrap();
                 tx.execute("INSERT INTO title (title) VALUES (?1)", params![alias]).unwrap();
@@ -94,5 +104,16 @@ pub async fn add_alias_with_title(conn: &Connection, title: String, alias: Strin
                 Ok(())
             }
         }
+    }).await
+}
+
+pub async fn remove_title(conn: &Connection, title: String) -> Result<(), DBError> {
+    conn.call(move |conn| {
+        let tx = conn.transaction().unwrap();
+        let title_id = tx.query_row("SELECT id FROM title WHERE title = (?1)", params![title], |row| row.get::<usize, u64>(0)).unwrap();
+        tx.execute("DELETE FROM url_title WHERE title_id = (?1)", params![title_id]).unwrap();
+        tx.execute("DELETE FROM title WHERE id = (?1)", params![title_id]).unwrap();
+        tx.commit().unwrap();
+        Ok(())
     }).await
 }
