ea677d1eeba357c06c0d47ecca2b92e120ef1a0b
Forte-NaoP <bae9750@gmail.com>
2023-03-27 00:33:03 +0900

    Support multiple playing methods
    - play single url from command option
    - play some urls from client queue

--- src/command_handler/commands/play.rs
+++ src/command_handler/commands/play.rs
@@ -1,4 +1,6 @@
-use regex::Regex;
+use std::{vec, collections::VecDeque, time::Duration};
+
+use tokio::time::sleep;
 use serenity::{
     async_trait,
     builder::CreateApplicationCommand,
@@ -22,6 +24,9 @@ use songbird::{
     Songbird,
     input,
     tracks::create_player,
+    EventHandler as VoiceEventHandler,
+    EventContext,
+    Event, TrackEvent,
 };
 
 use crate::{
@@ -29,17 +34,30 @@ use crate::{
         command_handler::*,
         command_data::*,
         command_return::CommandReturn,
-    }
+    },
+    MusicQueue,
 };
 
-struct Help;
+struct TrackEndNotifier;
+
+#[async_trait]
+impl VoiceEventHandler for TrackEndNotifier {
+    async fn act(&self, ctx: &EventContext<'_>) -> Option<Event> {
+        if let EventContext::Track(track_list) = ctx {
+            println!("Song has ended: {:?}", track_list);
+        }
+        None
+    }
+}
+
+struct Play;
 
 pub fn command() -> Box<dyn CommandInterface + Sync + Send> {
-    Box::new(Help)
+    Box::new(Play)
 }
 
 #[async_trait]
-impl CommandInterface for Help {
+impl CommandInterface for Play {
     async fn run(
         &self, 
         ctx: &Context, 
@@ -47,19 +65,38 @@ impl CommandInterface for Help {
         options: &[CommandDataOption]
     ) -> CommandReturn {
 
-        let url = Option::<String>::from(DataWrapper::from(options, 0)).unwrap();
-
-        let url_regex = Regex::new(r"^https?://(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&/=]*)$").unwrap();
-
-        if !url_regex.is_match(url.as_str()) {
-            return CommandReturn::String("주소 좀 똑바로 쳐라".to_owned())
-        }
-
         let gid = command.guild_id.unwrap();
         let voice_manager = songbird::get(ctx).await.expect("Songbird Voice client placed in at initialisation.");
+        let queue = ctx.data.read().await.get::<MusicQueue>().unwrap().clone();
 
-        if let Some(handler_lock) = voice_manager.get(gid) {
-            let mut handler = handler_lock.lock().await;
+        let mut play_time = 0;
+        let mut start_time = 0;
+        
+        let url = match Option::<String>::from(DataWrapper::from(options, 0)) {
+            Some(url) => {
+                play_time = match Option::<i64>::from(DataWrapper::from(options, 1)) {
+                    Some(play_time) => play_time,
+                    None => 0,
+                };
+        
+                start_time = match Option::<i64>::from(DataWrapper::from(options, 2)) {
+                    Some(start_time) => start_time,
+                    None => 0,
+                };
+
+                Some(url)
+            },
+            None => if queue.read().await.len() == 0 {
+                return CommandReturn::String("큐가 비어있습니다.\n큐에 곡을 넣거나 url을 입력해주세요.".to_string())
+            } else {
+                None
+            },
+        };
+
+        let mut handler_lock = voice_manager.get(gid).unwrap();
+        let mut handler = handler_lock.lock().await;
+        
+        if let Some(url) = url {
             let src = match input::Restartable::ytdl(url, true).await {
                 Ok(src) => src,
                 Err(why) => {
@@ -68,11 +105,25 @@ impl CommandInterface for Help {
                 }
             };
             let (mut audio, audio_handle) = create_player(src.into());
-
-            println!("{}", handler.current_channel().unwrap());
-            audio_handle.enable_loop().expect("restartable err");
+            audio_handle.add_event(Event::Track(TrackEvent::End), TrackEndNotifier).unwrap();
             handler.play(audio);
-            
+            sleep(Duration::from_secs(30)).await;
+            handler.stop();
+        } else {
+            while let Some(song_url) = queue.write().await.pop_front() {
+                let src = match input::Restartable::ytdl(song_url, true).await {
+                    Ok(src) => src,
+                    Err(why) => {
+                        println!("Err starting source: {:?}", why);
+                        return CommandReturn::String("재생 못함".to_owned())
+                    }
+                };
+                let (mut audio, audio_handle) = create_player(src.into());
+                audio_handle.add_event(Event::Track(TrackEvent::End), TrackEndNotifier).unwrap();
+                handler.play(audio);
+                sleep(Duration::from_secs(30)).await;
+                handler.stop();
+            }
         }
 
         CommandReturn::String("와 정말 알고 싶던 정보였어요".to_owned())
@@ -90,7 +141,21 @@ impl CommandInterface for Help {
                     .name("url")
                     .description("재생하고 싶은 노래 url")
                     .kind(CommandOptionType::String)
-                    .required(true)
+                    .required(false)
+            })
+            .create_option(|option| {
+                option
+                    .name("재생시간")
+                    .description("재생시간")
+                    .kind(CommandOptionType::Integer)
+                    .required(false)
+            })
+            .create_option(|option| {
+                option
+                    .name("시작시간")
+                    .description("시작시간(초)")
+                    .kind(CommandOptionType::Integer)
+                    .required(false)
             })
     }
 }
