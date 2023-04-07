ba357cddebffb26dc34e3941ba033dbd0c389e6b
Forte-NaoP <bae9750@gmail.com>
2023-04-07 23:17:35 +0900

    Implement command insert url to queue

--- src/command_handler/commands/insert_queue.rs
+++ src/command_handler/commands/insert_queue.rs
@@ -22,8 +22,8 @@ use serenity::{
 };
 use songbird::{
     Songbird,
-    input,
-    tracks::create_player,
+    input::{self, Input},
+    tracks::TrackHandle,
     Call,
 };
 
@@ -32,7 +32,9 @@ use crate::{
         command_handler::*,
         command_data::*,
         command_return::CommandReturn,
-    },
+    }, 
+    utils::{audio_module::youtube_dl::ytdl, guild_queue},
+    GuildQueueContainer,
 };
 
 struct InsertQueue;
@@ -55,10 +57,20 @@ impl CommandInterface for InsertQueue {
             None => return CommandReturn::String("url을 입력해주세요.".to_string()),
         };
 
-        let start_imidately = match Option::<bool>::from(DataWrapper::from(options, 1)) {
-            Some(start_imidately) => start_imidately,
-            None => false,
-        };
+        let gid = command.guild_id.unwrap();
+        let guild = ctx.cache.guild(gid).unwrap();
+
+        {        
+            let data = ctx.data.read().await;
+            let data = data.get::<GuildQueueContainer>().unwrap();
+            let queue = data.get(&gid).unwrap();
+            match queue.try_write() {
+                Ok(mut queue) => {
+                    queue.url_queue.push_back(url.clone());
+                },
+                Err(_) => return CommandReturn::String("큐가 사용 중 입니다.".to_owned()),
+            };
+        }
 
         CommandReturn::String(format!("{}를 큐에 추가했습니다.", url))
     }
@@ -77,41 +89,5 @@ impl CommandInterface for InsertQueue {
                     .kind(CommandOptionType::String)
                     .required(true)
             })
-            .create_option(|option| {
-                option
-                    .name("바로 재생")
-                    .description("큐에 추가하고 바로 재생합니다. (Default: false)")
-                    .kind(CommandOptionType::Boolean)
-                    .required(false)
-            })
     }
-}
-
-// guildqueue.url_queueu에서 src_queue로 ytdl로 input 넣기
-// 단 src_queue는 최대 10개 까지
-// 재생을 시작하면 스레드 하나 만들어서 무한루프 돌면서 src_queue 길이 10인지 체크하면서 url_queue에서 가져오기
-// 재생하는 곳에서는 src_queue에서 enqueue_source로 재생큐에 넣기
-// 모든 재생 끝나면 src_queue 스레드 종료 시키기
-
-
-async fn convert_url_to_src(ctx: &Context, gid: GuildId, voice_manager: &Songbird) {
-    use crate::utils::audio_module::youtube_dl;
-    use crate::GuildQueueContainer;
-    
-    let _gqc = ctx.data.read().await;
-    let gqc = _gqc.get::<GuildQueueContainer>().unwrap();
-
-    let _gq = gqc.write().await;
-    let gq = _gq.get_mut(&gid).unwrap();
-
-    let handler = voice_manager.get(gid).unwrap();
-    let handler_lock = handler.lock().await;
-
-    while handler_lock.queue().len() < 10 {
-        let url = gq.url_queue.pop_front().unwrap();
-        let src = youtube_dl::ytdl(url).await.unwrap();
-        handler_lock.enqueue_source(src);
-    }
-
-
 }
